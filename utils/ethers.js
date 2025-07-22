5ebd280b3e85408a8d5d78aa9ebd2bbe918b4322
RocBoi <rocboiquezworld2@gmail.com>
2025-07-22 17:31:25 -0400

    Update ethers.js

--- utils/ethers.js
+++ utils/ethers.js
@@ -7,7 +7,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   provider = new ethers.BrowserProvider(window.ethereum);
   signer = await provider.getSigner();
 } else {
-  provider = new ethers.JsonRpcProvider(
+  provider = new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/d13a1e95c6904ef5be8d143ccc533a53*); 
   signer = provider.getSigner(); // Will be null if not connected
 }
 
