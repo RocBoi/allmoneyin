c72ac30f4c0270ac70930b2e05afcddea4c8095f
RocBoi <rocboiquezworld2@gmail.com>
2025-07-23 13:53:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const path = require("path");
-require("dotenv").config();
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import nftVideoRoutes from './routes/nftvideo.routes.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
-
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/users", require("./routes/user.routes"));
-app.use("/api/nft", require("./routes/nft.routes"));
-app.use("/api/payment", require("./routes/payment.routes"));
-app.use("/api/upload", require("./routes/upload.routes"));
-app.use("/api/admin", require("./routes/admin.routes"));
-app.use("/api/subscriber", require("./routes/subscriber.routes"));
+app.use('/nft', nftVideoRoutes); // NFT Video Streaming
 
+app.get('/', (req, res) => {
+  res.send('<h1>🎧 Welcome to GCode NFT Auction Platform</h1><a href="/nft/NFTvideoBid">🎥 View DumbBootyNFT</a>');
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 GCode NFT Auction Server running at http://localhost:${PORT}`);
 });
