7795f56e200269b77ed6aba22f2c812f6ad0242b
RocBoi <rocboiquezworld2@gmail.com>
2025-07-23 13:55:50 -0400

    Create IPFS.js

---
+++ utils/IPFS.js
@@ -0,0 +1,3 @@
+// utils/ipfs.js
+export const IPFS_VIDEO_CID = "bafybeiclbirhq4zwyska56flarpqde73pckwsdiemc4gju2ab27iz2kene";
+export const IPFS_VIDEO_URL = `https://${IPFS_VIDEO_CID}.ipfs.w3s.link/`;
